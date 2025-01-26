a32bcfa0107746c1164352002321d3d733c41a68
iggyvillar <95318026+iggyvillar@users.noreply.github.com>
2025-01-26 14:42:43 -0500

    Update iggyBitcoinTracker.ino
    update rtos functionality, tasks, mutex

--- iggyBitcoinTracker.ino
+++ iggyBitcoinTracker.ino
@@ -14,18 +14,19 @@
 #define OLED_HEIGHT 64            //ssd1306 px height
 #define OLED_RESET -1
 #define OLED_ADDRESS 0x3C         //default for most ssd1306
-#define HTTPS_PORT 443
 
 Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
 
-const String apiURL = "http://api.coindesk.com/v1/bpi/currentprice/BTC.json";
+const String apiURL = "http://api.coindesk.com/v1/bpi/currentprice/BTC.json";   //api URL to fetch current BTC price
 
-const char* networkName = "";                     //replace with network name
+const char* networkName = "";                     //replace with network ssid
 const char* networkPassword = "";                 //replace with network password
 
 WiFiClient client;      //to make wifi connections
 HTTPClient http;        //for http get requests
 
+String btcPrice = "Fetching BTC price...";            //variable to hold bitcoin price
+SemaphoreHandle_t xMutex;                             //mutex to synchronize access to BTC price
 
 void setup() {
   Serial.begin(115200);     //initialize serial communication
@@ -40,70 +41,80 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.println("Connecting to Wi-Fi...");
-  display.display();
+  display.display();          //display message
 
-  //connect to wifi
-  WiFi.begin(networkName, networkPassword);
-  Serial.print("Connecting to Network");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("."); // Print dots while waiting for connection.
-  }
-  Serial.println();
+  xMutex = xSemaphoreCreateMutex();     //create mutex for synchronizing access to data
 
-  //show connection success
-  display.println("Connected to ");
-  display.print(networkName);
-  display.display();
-  delay(1500);
-  display.clearDisplay();
-
+  //create tasks for different functionalaties
+  xTaskCreatePinnedToCore(connectToWifi, "WiFiTask", 4096, NULL, 1, NULL, 0);     //task for connecting to wifi
+  xTaskCreatePinnedToCore(getBTCPrice, "APITask", 4096, NULL, 2, NULL, 1);        //task for fetching updated BTC price
+  xTaskCreatePinnedToCore(updateDisplay, "DisplayTask", 4096, NULL, 1, NULL, 1);  //task for updating oled display
 }
 
-void loop() {
-  Serial.print("Connecting to");
-  Serial.println(apiURL);
+void loop() { 
+}
 
-  //GET request for current BTC price in USD
-  http.begin(apiURL);
-  int httpCode = http.GET();      //perform GET request
-  StaticJsonDocument<2000> JSONDOC;
-  DeserializationError error = deserializeJson(JSONDOC, http.getString());
-  //JSON parsing handling
-  if (error){
-    Serial.print(F("Json parsing failed"));
-    Serial.println(error.f_str());
-    delay(2500);
-    return;
+//function to connect to wifi
+void connectToWifi(void* parameter) {
+  display.clearDisplay();                                         //clear display
+  display.println("Connecting to Wi-Fi...");
+  display.display();                                              //display previous line on oled
+
+  WiFi.begin(networkName, networkPassword);                       //begin wifi connection
+  while (WiFi.status() != WL_CONNECTED) {                         //wait until wifi is connected
+    delay(500);
+    Serial.print(".");
   }
 
-  //send status code to serial monitor for debugging
-  Serial.print("HTTP Status Code:");
-  Serial.println(httpCode);
+  Serial.println("Wi-Fi Connected");                              //print success message in serial terminal
+  display.clearDisplay();                                         //clear display on oled
+  display.println("Wi-Fi Connected");
+  display.display();                                              //print success message on SSD1306
+  delay(1000);                                                    //delay before deleting task
 
-  //store BTC price in string
-  String currentBTCprice = JSONDOC["bpi"]["USD"]["rate_float"].as<String>();
-  http.end();
+  vTaskDelete(NULL);                                              //delete this task after Wi-Fi connection
+}
 
-  //display results on oled
+//task for fetching current BTC price
+void getBTCPrice(void* parameter) {
+  while (true) {
+    if (WiFi.status() == WL_CONNECTED) {                                //check if wifi is connected
+      http.begin(apiURL);                                               //initialize http request
+      int httpCode = http.GET();                                        //get request
+      if (httpCode == 200) {                                            //if response code is 200 indicating success
+        String payload = http.getString();                              //
+        StaticJsonDocument<2000> doc;
+        DeserializationError error = deserializeJson(doc, payload);     //parse json
+        if (!error) {                                                   //check json parsing status
+          String price = doc["bpi"]["USD"]["rate"].as<String>();        //extract BTC price
+          xSemaphoreTake(xMutex, portMAX_DELAY);                        //take mutex to access btc price
+          btcPrice = "$" + price;                                       //update var
+          xSemaphoreGive(xMutex);                                        //release mutex
+        }
+      }
+      http.end();                                                        //end http req
+    }
+    vTaskDelay(10000 / portTICK_PERIOD_MS);                              //wait 10 seconds until next fetch
+  }
+}
 
-  //display BTC header
-  display.clearDisplay();
 
+//function for updating display 
+void updateDisplay(void* parameter) {
+  while (true) {
+    xSemaphoreTake(xMutex, portMAX_DELAY);              //take mutex for safe access to btc price  
+    String price = btcPrice;                            //get latest BTC price
+    xSemaphoreGive(xMutex);                             //release mutex
 
-  display.setTextSize(1);
-  displayCentered("BTC/USD", 0, 0);
+    display.clearDisplay();                                                 //clear display
+    display.setTextSize(1);
+    displayCentered("BTC/USD", 0, 0);
+    displayCentered(price, 0, 25);                                          //display BTC price
+    displayCentered("IGGY'S ESP32 PROJECT", 0, 55);                    //display project name
+    display.display();
 
-  //display current price of BTC
-  displayCentered("$" + currentBTCprice, 0, 25);
-
-  //display project name
-  display.setTextSize(1);
-  displayCentered("IGGY'S ESP32 PROJECT", 0, 55); //change text here
-
-  display.display();
-  http.end();
-  delay(10000);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);                              //update every second
+  }
 }
 
 void displayCentered(const String txBuffer, int x, int y){
